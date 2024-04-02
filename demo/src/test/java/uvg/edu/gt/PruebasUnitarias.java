f69c8a99d4e7231d2a75ddcddf3f697395ae97fe
amb231143 <amb231143@uvg.edu.gt>
2024-04-02 15:25:41 -0600

    .
    .

--- demo/src/test/java/uvg/edu/gt/PruebasUnitarias.java
+++ demo/src/test/java/uvg/edu/gt/PruebasUnitarias.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 
-public class BinaryTreeTest {
+public class PruebasUnitarias {
 
     @Test
     public void testInsertar() {
